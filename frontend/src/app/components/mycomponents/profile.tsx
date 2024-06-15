d52763e61386f4e23bcf5c89f50fcf3e20a312b6
Ibrahim abdulkarim <aliveibi080@gmail.com>
2024-06-15 12:20:50 +0100

    Update different files

--- frontend/src/app/components/mycomponents/profile.tsx
+++ frontend/src/app/components/mycomponents/profile.tsx
@@ -8,37 +8,47 @@ import {
   } from "@starknet-react/core";
 import Header from "../Header";
 
-const Profile = () => {
-    
-    const {account, address, status } = useAccount()
 
-    
+
+import Image from "next/image";
+
+const Profile = () => {
+    const { account, address, status } = useAccount();
 
     return (
-        <>
-            <Header />
-            <div className="flex flex-col items-center justify-center min-h-screen p-4">
-                <h2 className="text-white mb-4">
-                    {account ? `Welcome ${address}` : "Connect your StarkNet wallet"}
-                </h2>
-                {!account && (
-                    <button
-                     //   onClick={connect}
-                        className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-700"
-                    >
-                        Connect Wallet
-                    </button>
-                )}
-                {account && (
-                    <button
-                    //    onClick={disconnect}
-                        className="bg-red-500 text-white p-2 rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-700"
-                    >
-                        Disconnect Wallet
-                    </button>
+
+       <>
+        <Header />
+       
+        <div>
+                {!account ? (
+                    <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
+           
+           
+                    <div className="text-center text-white mb-8">
+                        <h1 className="text-4xl md:text-6xl font-bold mb-4 animate-bounce">Welcome to Seeker</h1>
+                        <p className="text-lg md:text-2xl font-light">
+                            Connect your wallet to explore an array of opportunities in the Starknet ecosystem!
+                        </p>
+                    </div>
+                    <div className="mb-8">
+                        <Image
+                            src="/myimages/world2.jpg"
+                            alt="Starknet Ecosystem"
+                            width={800}
+                            height={400}
+                            className="rounded-lg shadow-lg cursor-pointer"
+                            onLoad={() => console.log('Image loaded')}
+                        />
+                    </div>
+                </div>
+                ) : (
+                    <div className="text-white" >
+                        <h2>Welcome to Dashboard!</h2>
+                        </div>
                 )}
             </div>
-        </>
+       </>
     );
 }
 
