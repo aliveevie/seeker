3c35e322cb4546cbf467b9d4d767eaafabbb506d
Ibrahim abdulkarim <aliveibi080@gmail.com>
2024-06-15 06:19:33 +0100

    Create a SignIn page

---
+++ frontend/src/app/signin/page.tsx
@@ -0,0 +1,10 @@
+import SignIn from "../components/mycomponents/signIn";
+
+
+export default async function Page() {
+  return (
+    <div className="container mx-auto py-10">
+        <SignIn />
+    </div>
+  );
+}
